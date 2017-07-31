04ac67232ce390ab24ddc43b8582d4c29b1f2a44
sophiawang4 <sophia.wang444@gmail.com>
2017-07-31 15:35:54 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,14 +1,20 @@
+var box1 = document.getElementById("box1");
+box1.addEventListener("mouseenter", function(){
+  box1.style.backgroundColor = "pink";
+});
 
+var box2 = document.getElementById("box2");
+box2.addEventListener("mouseout", function(){
+  box2.style.backgroundColor = "orange";
+});
 
 
 //how to change color everytime mouse touches button
-
 var box3 = document.getElementById("box3");
-box3.addEventListener("My bucket list", function(){
+box3.addEventListener("mouseenter", function(){
   box3.style.backgroundColor = "pink";
 });
-box3.addEventListener("My bucket list", function(){
+box3.addEventListener("mouseout", function(){
   box3.style.backgroundColor = "orange";
 });
-
 
