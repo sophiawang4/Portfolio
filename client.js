9c33e8743bbe081b70104a4a36e5041815c4cdbd
sophiawang4 <sophia.wang444@gmail.com>
2017-07-31 14:09:21 -0400

    Create client.js

---
+++ client.js
@@ -0,0 +1,26 @@
+// ********************** MOUSE OVER ***********************
+
+//how to make mouse change color when mouse is over button (the initial color of the button is on CSS)
+
+var box1 = document.getElementById("box1");
+box1.addEventListener("mouseenter", function(){
+  box1.style.backgroundColor = "pink";
+});
+
+var box2 = document.getElementById("box2");
+box2.addEventListener("mouseout", function(){
+  box2.style.backgroundColor = "orange";
+});
+
+
+//how to change color everytime mouse touches button
+
+var box3 = document.getElementById("box3");
+box3.addEventListener("mouseenter", function(){
+  box3.style.backgroundColor = "pink";
+});
+box3.addEventListener("mouseout", function(){
+  box3.style.backgroundColor = "orange";
+});
+
+
