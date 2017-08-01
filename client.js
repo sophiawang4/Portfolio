e3b4c64fbf36e0fd7372c5e360340d9bbea7dc67
sophiawang4 <sophia.wang444@gmail.com>
2017-08-01 09:17:09 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,89 +1,45 @@
 
+// server.js
+// where your node app starts
 
+// init project
+var express = require('express');
+var app = express();
 
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
 
-// ********************** CLICK ME BUTTON ***********************
-// now, let's do stuff to our button -- to do this, we need an event listener
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
 
-// let's get the element that we want to "listen" for
-var btnElement = document.getElementById("first-btn");
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
 
-// now we want to attach an event listener to this element
-// this is doing a few things:
-// 1. Add the event listener to the greetingElement
-// 2. We're "listening" for the 'click' event
-// 3. When the button gets clicked, the function will be called
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
 
-// so this is the general format (ps: there are more ways to do this!!!):
-// element.addEventListener("event", callback function)
-
-btnElement.addEventListener("click", function(){
-  document.getElementById("btn-text").innerHTML = "you clicked me, thanks!";
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
 
 
-
-
-
-
-
-
-// ********************** COLORFUL BUTTON ***********************
-// we can also edit styles with js, and do some operations
-var colorButton = document.getElementById("color-btn");
-
-colorButton.addEventListener("click", function(){
-  var colors = ["blue", "pink", "hotpink", "purple", "lightblue", "aqua", "teal", "fuchsia", "lightpink", "orange", "green", "yellow", "maroon", "black"];
-  var x = Math.floor((Math.random() * colors.length) + 1);
-  colorButton.style.backgroundColor = colors[x];
-});
-
-
-
-
-
-// ********************** TOGGLE BUTTON ***********************
-
-var toggleBtn = document.getElementById("toggle-btn");
-toggleBtn.addEventListener("click", myToggleFunction);
-
-function myToggleFunction() {
-    var x = document.getElementById('myBio');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-        toggleBtn.innerHTML = "hide my bio";
-    } else {
-        x.style.display = 'none';
-        toggleBtn.innerHTML = "show my bio";
-    }
-}
-
-
-
-
-
-
-
-// ********************** MOUSE OVER ***********************
-var box1 = document.getElementById("box1");
-box1.addEventListener("mouseenter", function(){
-  box1.style.backgroundColor = "pink";
-});
-
-var box2 = document.getElementById("box2");
-box2.addEventListener("mouseout", function(){
-  box2.style.backgroundColor = "orange";
-});
-
-var box3 = document.getElementById("box3");
-box3.addEventListener("mouseenter", function(){
-  box3.style.backgroundColor = "pink";
-});
-box3.addEventListener("mouseout", function(){
-  box3.style.backgroundColor = "orange";
-});
 
 
 
