081cde6977b1de357a359e1fd6fa1cca458dadb1
sophiawang4 <sophia.wang444@gmail.com>
2017-08-02 09:14:31 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,88 +1,106 @@
+// client-side js
+// run by the browser each time your view template is loaded
 
-/* styles */
-/* called by your view template */
 
-/* You might want to try something fancier: */
-/* less: http://lesscss.org/ */
 
-* {
-  box-sizing: border-box;
+
+
+// ******************** hello, javascript! ************************
+// this is an example of selecting an element bu it's id, and then editing what goes inside
+console.log(document.getElementById("greeting").innerHTML);
+console.log("*******");
+
+document.getElementById("greeting").innerHTML = "hello, javascript!";
+
+console.log(document.getElementById("greeting").innerHTML);
+
+
+
+
+
+
+
+
+// ********************** CLICK ME BUTTON ***********************
+// now, let's do stuff to our button -- to do this, we need an event listener
+
+// let's get the element that we want to "listen" for
+var btnElement = document.getElementById("first-btn");
+
+// now we want to attach an event listener to this element
+// this is doing a few things:
+// 1. Add the event listener to the greetingElement
+// 2. We're "listening" for the 'click' event
+// 3. When the button gets clicked, the function will be called
+
+// so this is the general format (ps: there are more ways to do this!!!):
+// element.addEventListener("event", callback function)
+
+btnElement.addEventListener("click", function(){
+  document.getElementById("btn-text").innerHTML = "you clicked me, thanks!";
+});
+
+
+
+
+
+
+
+
+// ********************** COLORFUL BUTTON ***********************
+// we can also edit styles with js, and do some operations
+var colorButton = document.getElementById("color-btn");
+
+colorButton.addEventListener("click", function(){
+  var colors = ["blue", "pink", "hotpink", "purple", "lightblue", "aqua", "teal", "fuchsia", "lightpink", "orange", "green", "yellow", "maroon", "black"];
+  var x = Math.floor((Math.random() * colors.length) + 1);
+  colorButton.style.backgroundColor = colors[x];
+});
+
+
+
+
+
+// ********************** TOGGLE BUTTON ***********************
+
+var toggleBtn = document.getElementById("toggle-btn");
+toggleBtn.addEventListener("click", myToggleFunction);
+
+function myToggleFunction() {
+    var x = document.getElementById('myBio');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+        toggleBtn.innerHTML = "hide my bio";
+    } else {
+        x.style.display = 'none';
+        toggleBtn.innerHTML = "show my bio";
+    }
 }
 
-body {
-  font-family: helvetica, arial, sans-serif;
-  margin: 25px;
-}
 
-#greeting{
-  color: red;
-}
 
-.party{
-  color: purple;
-}
 
-h1 {
-  font-weight: bold;
-  color: pink;
-  text-align: center;
-}
 
-h2{
-  text-align: center;
-}
 
-.bold {
-  font-weight: bold;
-}
 
-p {
-  max-width: 600px;
-}
+// ********************** MOUSE OVER ***********************
+var box1 = document.getElementById("box1");
+box1.addEventListener("mouseenter", function(){
+  box1.style.backgroundColor = "pink";
+});
 
-.centered{
-  text-align: center
-}
+var box2 = document.getElementById("box2");
+box2.addEventListener("mouseout", function(){
+  box2.style.backgroundColor = "orange";
+});
 
-button {
-  font-size: 16px;
-  border-radius: 3px;
-  background-color: lightgrey;
-  border: 1px solid grey;
-  box-shadow: 2px 2px teal;
-  cursor: pointer;
-  margin-top: 40px;
-  margin-bottom: 40px;
-}
+var box3 = document.getElementById("box3");
+box3.addEventListener("mouseenter", function(){
+  box3.style.backgroundColor = "pink";
+});
+box3.addEventListener("mouseout", function(){
+  box3.style.backgroundColor = "orange";
+});
 
-button:hover {
-  background-color: yellow;
-}
 
-button:active {
-  box-shadow: none;
-}
 
-div#box1{
-  background-color: orange;
-  padding: 20px;
-  text-align: center;
-  color: white;
-  margin: 10px;
-}
-
-div#box2{
-  background-color: pink;
-  padding: 20px;
-  text-align: center;
-  color: white;
-  margin: 10px;
-}
-
-div#box3{
-  background-color: orange;
-  padding: 20px;
-  text-align: center;
-  color: white;
-  margin: 10px;
-}
